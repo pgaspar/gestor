114c876071bdd62ef487cb0451806e9ee3f94230
alcides <alcides@98cf0b1a-8b52-0410-b8cf-cedd4b1de3d6>
2008-07-23 16:05:05 +0000

    BD com o pedrogaspar
    git-svn-id: http://code.alcidesfonseca.com/gestor/jksite@6 98cf0b1a-8b52-0410-b8cf-cedd4b1de3d6

--- gestor/views.py
+++ gestor/views.py
@@ -3,5 +3,5 @@ from gestor.models import Project
 from django.contrib.auth.models import User
 
 def project_list(request):
-	#p = Project.objects.filter(workers=request.user)
+	p = Project.objects.filter(workers=request.user)
 	return render_to_response('project_detail.html',{'object_list':p})
