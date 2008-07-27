390e2ebf625ca78cc51f27ae32cb331b4c271ad2
alcides <alcides@98cf0b1a-8b52-0410-b8cf-cedd4b1de3d6>
2008-07-27 16:23:53 +0000

    git-svn-id: http://code.alcidesfonseca.com/gestor/jksite@32 98cf0b1a-8b52-0410-b8cf-cedd4b1de3d6

--- gestor/admin.py
+++ gestor/admin.py
@@ -1,14 +1,22 @@
 from gestor.models import Project, ActionItem, Note
 from django.contrib import admin
 
-class ProjectAdmin(admin.ModelAdmin):
-	
+class ProjectAdmin(admin.ModelAdmin):	
 	list_filter = ('active','manager','team','start_date','end_date',)
 	search_fields = ['name','description']
 
 admin.site.register(Project,ProjectAdmin)
 
+class ActionItemAdmin(admin.ModelAdmin):	
+	list_filter = ('targets','due_date','done','project',)
+	search_fields = ['title','description','project']
 
-admin.site.register(ActionItem)
 
-admin.site.register(Note)
+admin.site.register(ActionItem,ActionItemAdmin)
+
+class NoteAdmin(admin.ModelAdmin):	
+	list_filter = ('project','author',)
+	search_fields = ['title','description','project']
+
+
+admin.site.register(Note,NoteAdmin)
