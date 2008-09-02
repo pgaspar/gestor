716ca2d38a54f156585b5ee8d022c5e8ca73654a
pedrogaspar <pedrogaspar@98cf0b1a-8b52-0410-b8cf-cedd4b1de3d6>
2008-09-02 12:44:53 +0000

    Finalmente... Commit de tudo o que prometi na versão 54.
    git-svn-id: http://code.alcidesfonseca.com/gestor/jksite@57 98cf0b1a-8b52-0410-b8cf-cedd4b1de3d6

---
+++ curriculos/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class CurriculumVitae(models.Model):
+    owner = models.OneToOneField(User)
+    adress = models.CharField(max_length=100)
+    homepage = models.CharField(max_length=40)
+    phone = models.CharField(max_length=9)
+    
+    course = models.CharField(max_length=40)
+    coruse_year = models.IntegerField()
+    
+    complements = models.Text(max_length=1000)
+    foreign_langs = models.Text(max_length=1000)
+    computer_skills = models.Text(max_length=1000)
+    other_skills = models.Text(max_length=1000)
+    interests = models.Text(max_length=1000)
+    proficient_areas = models.Text(max_length=1000)
+    
+    set_date = models.DateField(auto_now_add=True)
+    
+    
+    def __unicode__(self):
+        return u"%s's Curriculum" % self.owner.get_full_name()
+    
+    def get_abolute_url(self):
+        return "/users/%s/curriculum" % self.owner.username
---
+++ curriculos/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render_to_response, get_object_or_404
+from django.contrib.auth.models import User
+from django.template.context import RequestContext
+from curiculos.models import CurriculumVitae
+
+def curriculum(request,username):
+    u = get_object_or_404(User, username = username)
+    c = get_object_or_404(CurriculumVitae, owner = u)
+    return render(request,'curriculum.html',{'u':u, 'cv':c})
