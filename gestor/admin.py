2353ce1c480ef51a8c4b9bedb9c7073ff0860fc7
alcides <alcides@98cf0b1a-8b52-0410-b8cf-cedd4b1de3d6>
2008-09-08 16:04:08 +0000

    Added list_display to gestor.admin
    git-svn-id: http://code.alcidesfonseca.com/gestor/jksite@80 98cf0b1a-8b52-0410-b8cf-cedd4b1de3d6

--- gestor/admin.py
+++ gestor/admin.py
@@ -2,12 +2,14 @@ from gestor.models import Project, ActionItem, Note, File
 from django.contrib import admin
 
 class ProjectAdmin(admin.ModelAdmin):	
+	list_display = ('name','manager','active','start_date',)	
 	list_filter = ('active','manager','team','start_date','end_date',)
 	search_fields = ['name','description']
 
 admin.site.register(Project,ProjectAdmin)
 
 class ActionItemAdmin(admin.ModelAdmin):	
+	list_display = ('title','project','done','due_date')
 	list_filter = ('project','done','targets',)
 	search_fields = ['title','description','project']
 
@@ -15,6 +17,7 @@ class ActionItemAdmin(admin.ModelAdmin):
 admin.site.register(ActionItem,ActionItemAdmin)
 
 class NoteAdmin(admin.ModelAdmin):	
+	list_display = ('title','project','author',)
 	list_filter = ('project','author',)
 	search_fields = ['title','description','project']
 
@@ -22,6 +25,7 @@ class NoteAdmin(admin.ModelAdmin):
 admin.site.register(Note,NoteAdmin)
 
 class FileAdmin(admin.ModelAdmin):	
+	list_display = ('title','project','author',)
 	list_filter = ('project','author',)
 	search_fields = ['title','project']
 
