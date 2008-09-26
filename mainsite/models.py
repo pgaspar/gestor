c769b5bf803035eabd09855ddac35595d54e1979
pedrogaspar <pedrogaspar@98cf0b1a-8b52-0410-b8cf-cedd4b1de3d6>
2008-09-26 22:03:30 +0000

    Added some stuff to the mainsite's models. Not tested. (Delete comments after reading)
    git-svn-id: http://code.alcidesfonseca.com/gestor/jksite@106 98cf0b1a-8b52-0410-b8cf-cedd4b1de3d6

--- mainsite/models.py
+++ mainsite/models.py
@@ -1,12 +1,25 @@
 from django.db import models
+from django.core.exceptions import PermissionDenied
+
 
 class Noticia(models.Model):
 	"""(Noticia description)"""
+	
+	author = models.ForeignKey(User, related_name='news_set')	# not sure about this related_name
+	title = models.CharField(max_length=200)
+	date = models.DateTimeField(auto_now=True)
+	body = models.TextField()
+	
 	
 
 	class Admin:
 		list_display = ('',)
 		search_fields = ('',)
+		# Textmate's budles ftw, right? :)
 
-	def __str__(self):
-		return "Noticia"
+	def __unicode__(self):
+		return u"%s" % self.title
+	
+	def isAuthor(self, user):
+		if not user is self.author or user.is_staff:
+			raise PermissionDenied()	# getting ready for the 'edit news' function. 
