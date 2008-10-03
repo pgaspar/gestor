ebd8822cc0ee645fd838de26342dcde78656a946
pedrogaspar <pedrogaspar@98cf0b1a-8b52-0410-b8cf-cedd4b1de3d6>
2008-10-03 12:34:37 +0000

    Added dates to the news bar.
    git-svn-id: http://code.alcidesfonseca.com/gestor/jksite@110 98cf0b1a-8b52-0410-b8cf-cedd4b1de3d6

--- mainsite/templatetags/news.py
+++ mainsite/templatetags/news.py
@@ -10,8 +10,9 @@ def latest_news():
 	
 	html = "<ul id='news_list'>\n"
 	for obj in latest:
-		html += """<li><a href='%s'>%s</a></li>\n""" %(obj.get_absolute_url(), obj.title)
+		html += """<li><a href='%s'>%s - %s</a></li>\n""" %(obj.get_absolute_url(), obj.date.strftime('%d-%m-%y'), obj.title)
 	
 	html +="</ul>"
 	return html
 #register.tag('latest_news', latest_news)
+
