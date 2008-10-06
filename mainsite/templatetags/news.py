13f2f49738a137ab5a28ba167e533d799ec74bcc
alcides <alcides@98cf0b1a-8b52-0410-b8cf-cedd4b1de3d6>
2008-10-06 10:05:36 +0000

    Changed 500 and 404 and Just last 5 news
    git-svn-id: http://code.alcidesfonseca.com/gestor/jksite@113 98cf0b1a-8b52-0410-b8cf-cedd4b1de3d6

--- mainsite/templatetags/news.py
+++ mainsite/templatetags/news.py
@@ -6,7 +6,7 @@ register = Library()
 @register.simple_tag
 def latest_news():
 	
-	latest = News.objects.order_by('-date')[:10]
+	latest = News.objects.order_by('-date')[:5]
 	
 	html = "<ul id='news_list'>\n"
 	for obj in latest:
