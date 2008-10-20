f18b8de8062f00c4613ac7c1cc32285fadb22846
alcides <alcides@98cf0b1a-8b52-0410-b8cf-cedd4b1de3d6>
2008-10-20 22:05:32 +0000

    Admin de News Restored, mas com but
    git-svn-id: http://code.alcidesfonseca.com/gestor/jksite@124 98cf0b1a-8b52-0410-b8cf-cedd4b1de3d6

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -2,8 +2,8 @@ from mainsite.models import News
 from django.contrib import admin
 
 class NewsAdmin(admin.ModelAdmin):	
-	#list_display = ('title','date',)	
+	list_display = ('title','date',)	
 	list_filter = ('date',)
 	search_fields = ['title','body']
 
-admin.site.register(News) #,NewsAdmin)
+admin.site.register(News, NewsAdmin)
