4174c76322ede58f1dc86153dd5d08a45ce7c52d
alcides <alcides@98cf0b1a-8b52-0410-b8cf-cedd4b1de3d6>
2008-10-20 21:57:09 +0000

    Changed date adding
    git-svn-id: http://code.alcidesfonseca.com/gestor/jksite@121 98cf0b1a-8b52-0410-b8cf-cedd4b1de3d6

--- mainsite/models.py
+++ mainsite/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+import datetime
+
 class News(models.Model):
 	""" Website News """
 	
 	title = models.CharField(max_length=200)
-	date = models.DateTimeField(auto_now=True)
+	date = models.DateTimeField()
 	body = models.TextField()
+	
+	def save(self):
+		if not self.id:
+			self.date = datetime.datetime.now()
+		super(News, self).save()
 	
 	def __unicode__(self):
 		return u"%s" % self.title
