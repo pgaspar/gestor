fba7d8fdc8fcb833506dcf551b6c263124f37b2d
pedrogaspar <pedrogaspar@98cf0b1a-8b52-0410-b8cf-cedd4b1de3d6>
2009-02-02 12:28:53 +0000

    Fixed some minor bugs:
    * Logout now points to /login/?next=/gestor/
    * The JK logo now points to /gestor/
    
    git-svn-id: http://code.alcidesfonseca.com/gestor/jksite@137 98cf0b1a-8b52-0410-b8cf-cedd4b1de3d6

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
-	(r'^logout/$', 'django.contrib.auth.views.logout_then_login',),
+	(r'^logout/$', 'django.contrib.auth.views.logout_then_login', {'login_url': '/accounts/login/?next=/gestor/'}),
 	(r'^profile/$', 'django.views.generic.simple.redirect_to', { 'url': "/" }), 
 	(r'^password_change/$', 'django.contrib.auth.views.password_change', {'template_name': 'password_change.html'}), 
 	(r'^password_change/done/$', 'django.contrib.auth.views.password_change_done', {'template_name': 'password_change_done.html'}), 
