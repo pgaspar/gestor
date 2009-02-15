b6fefdd2c119c08e45ca56164c9f761b5e40ff1e
Pedro Gaspar <pedro.gaxpar@gmail.com>
2009-02-15 22:40:35 +0000

    Changed some layout stuff.
    Added a footer to every widget (needs a nice background color).
    Replaced col_right and col_left with col as sugested by alcides.

--- gestor/templatetags/dashboard.py
+++ gestor/templatetags/dashboard.py
@@ -2,20 +2,22 @@ from django.template import Library
 
 register = Library()
 
+list_size = 3
+
 @register.filter
 def team_list(value):
 	
 	users = [value.manager]
-	for user in value.team.all()[:3]:
+	for user in value.team.all()[:list_size]:
 		if user != value.manager: users.append(user)
 	
-	return users[:3]
+	return users[:list_size]
 	
 @register.filter
 def more_members(value):
-	listed_members = 3
 	
-	if value.manager in value.team.all():
-		return len( value.team.all() ) > listed_members
+	team = value.team.all()
+	if value.manager in team:
+		return len( team ) > list_size
 	else:
-		return len( value.team.all() ) + 1 > listed_members
+		return len( team ) + 1 > list_size
