a7301a631cf46066b7e73aa1c3e11d57761f32dc
Pedro Gaspar <pedro.gaxpar@gmail.com>
2009-03-06 18:26:41 +0000

    Added the user's Todo List to the profile page.

--- accounts/views.py
+++ accounts/views.py
@@ -10,4 +10,5 @@ def profile(request,username):
 	u = get_object_or_404(User, username = username)
 	current_projects = u.projects_working.filter(active=True)
 	past_projects = u.projects_working.filter(active=False)
-	return render(request,'user_profile.html',{'u':u,'current_projects':current_projects,'past_projects':past_projects})
+	todo_list = User.objects.get(username=username).actionitem_todo.filter(done=False).order_by("due_date")
+	return render(request,'user_profile.html',{'u':u,'current_projects':current_projects,'past_projects':past_projects,'todo_list':todo_list})
