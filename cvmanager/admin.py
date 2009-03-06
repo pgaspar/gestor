ba65d5ea4e4f5906a4f45c97e99eb48ac0558a45
Pedro Gaspar <pedro.gaxpar@gmail.com>
2009-03-06 20:04:57 +0000

    Removed owner filtering from CV admin area.

--- cvmanager/admin.py
+++ cvmanager/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 class CurriculumVitaeAdmin(admin.ModelAdmin):	
 	list_display = ('owner','course','course_year','set_date',)	
-	list_filter = ('owner','course','course_year','set_date')
+	list_filter = ('course','course_year','set_date')
 	search_fields = ('owner','course')
 
 try: admin.site.register(CurriculumVitae,CurriculumVitaeAdmin)
