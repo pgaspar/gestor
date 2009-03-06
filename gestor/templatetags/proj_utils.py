844f2102f21eb568c63c49fb5e1e6a58321c486c
Pedro Gaspar <pedro.gaxpar@gmail.com>
2009-03-06 19:07:55 +0000

    Todo List in user profile no longer public.
    Only admins and that user's managers can see the list now.

--- gestor/templatetags/proj_utils.py
+++ gestor/templatetags/proj_utils.py
@@ -5,3 +5,7 @@ register = Library()
 @register.filter
 def ismanager(value, obj):
 	return value == obj.manager
+
+@register.filter
+def manages_user(request_user, u):
+	return request_user in [ proj.manager for proj in u.projects_working.filter(active=True) ]
