e7b779a25cdd39f0e5ada56a5d1cf13449e0be80
Pedro Gaspar <pedro.gaxpar@gmail.com>
2009-03-08 19:06:39 +0000

    Curriculum's set date fixed.

--- cvmanager/views.py
+++ cvmanager/views.py
@@ -10,6 +10,7 @@ from django.db.models import Q
 from cvmanager.forms import CvForm, CvFindForm
 from django.core.exceptions import PermissionDenied
 
+import datetime
 
 def render(request,template,context={}):
     return render_to_response(template,context,context_instance=RequestContext(request))
@@ -51,6 +52,9 @@ def edit_view(request,object_id,form_class,template_name):
         if form.is_valid():
             
             obj = form.save()
+            obj.set_date = datetime.date.today()
+            obj.save()
+            
             if request.user.is_authenticated():
                 request.user.message_set.create(message="The %s was updated" % model._meta.verbose_name )
             return HttpResponseRedirect(obj.get_absolute_url())
