74bc2fc745b6845d2ae5ea0a59738949a89f6f12
Alcides Fonseca <me@alcidesfonseca.com>
2009-03-11 15:07:20 +0000

    Tooltip in dashboard

--- gestor/templatetags/dashboard.py
+++ gestor/templatetags/dashboard.py
@@ -2,22 +2,11 @@ from django.template import Library
 
 register = Library()
 
-list_size = 3
-
 @register.filter
 def team_list(value):
 	
 	users = [value.manager]
-	for user in value.team.all()[:list_size]:
-		if user != value.manager: users.append(user)
-	
-	return users[:list_size]
-	
-@register.filter
-def more_members(value):
-	
-	team = value.team.all()
-	if value.manager in team:
-		return len( team ) > list_size
-	else:
-		return len( team ) + 1 > list_size
+	for user in value.team.all():
+		if user != value.manager:
+			users.append(user)
+	return users
