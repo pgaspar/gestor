7bc4ac81dcf2aa1eaaaa1cd87ddbb8fc2a7ac901
Pedro Gaspar <pedro.gaxpar@gmail.com>
2009-06-16 13:14:11 +0100

    Small Fixes.

--- media/js/formacao.js
+++ media/js/formacao.js
@@ -1,10 +1,11 @@
 function showPwdForm(){
 	$('password_form').show();
-	Form.focusFirstElement( $('password_form') );
+	$('password_form').focusFirstElement();
 	$('private_img').hide();
 }
 
 function hidePwdForm(){
 	$('password_form').hide();
 	$('private_img').show();
+	$('pwd_input').clear();
 }
