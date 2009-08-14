59cded364106c5f86550f196532a5bd0d9edaca7
pgaspar <pedro.gaxpar@gmail.com>
2009-08-14 02:06:49 +0100

    'New' icon added to Edit Profile link.

--- cvmanager/views.py
+++ cvmanager/views.py
@@ -18,54 +18,6 @@ import datetime
 def render(request,template,context={}):
 	return render_to_response(template,context,context_instance=RequestContext(request))
 
-# General Views
-"""
-def create_view(request,object_id,form_class,template_name):
-	model = form_class.Meta.model
-	u = get_object_or_404(User, username = object_id)
-	
-	if not request.user == u: raise PermissionDenied()
-	
-	if request.method == 'POST':
-		form = form_class(request.POST)
-		if form.is_valid():
-			obj = form.save()
-			if request.user.is_authenticated():
-				request.user.message_set.create(message="The %s was updated" % model._meta.verbose_name )
-			return HttpResponseRedirect(obj.get_absolute_url())
-	else:
-		form = form_class(initial={'owner':request.user.id})
-
-	return render(request,template_name,{'form':form})
-
-
-def edit_view(request,object_id,form_class,template_name):
-	model = form_class.Meta.model
-	u = get_object_or_404(User, username = object_id)
-	
-	if not request.user.username == u.username: raise PermissionDenied()
-
-	try:
-		obj = model.objects.get(owner = u)
-	except:
-		return create_view(request,object_id,form_class,template_name)
-	
-	if request.method == 'POST':
-		form = form_class(request.POST, request.FILES, instance=obj)
-		if form.is_valid():
-			
-			obj = form.save()
-			obj.set_date = datetime.date.today()
-			obj.save()
-			
-			if request.user.is_authenticated():
-				request.user.message_set.create(message="The %s was updated" % model._meta.verbose_name )
-			return HttpResponseRedirect(obj.get_absolute_url())
-	else:
-		form = form_class(instance=obj)
-	return render(request,template_name,{'form':form})
-"""
-
 # Curriculum Views
 
 @login_required
