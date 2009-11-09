32a07ba3da24f5ba4ee7cca8dd017c9c1c8f24fc
Pedro Gaspar <pedro.gaxpar@gmail.com>
2009-11-09 00:20:39 +0000

    Ordered the author select box by author username.

--- mainsite/forms.py
+++ mainsite/forms.py
@@ -1,11 +1,13 @@
 from django.forms import *
-from mainsite.models import News
+from mainsite.models import News, User
 from django.contrib.admin.widgets import AdminDateWidget
 
 class NewsForm(ModelForm):
 	body = CharField(widget=Textarea(),help_text="(supports <a href='http://hobix.com/textile/' target='_blank'>textile</a>)")
 	date = DateTimeField(widget=AdminDateWidget())
 	is_published = BooleanField(help_text="Only published posts will be visible to the outside users. However, it will be visible to administrators like yourself!", required=False)
+	
+	author = ModelChoiceField(queryset=User.objects.all().order_by('username'))
 	
 	class Meta:
 		model = News
